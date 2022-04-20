87a9985549de1e1867dd001005184d795262933f
udemezue uchegbu <udezmail@gmail.com>
2022-04-20 10:50:48 +0000

    this is the ASP.NET core Version

---
+++ Model/testdatabase21.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace databaseRest.Model
+{
+    public partial class testdatabase21 : DbContext
+    {
+        public testdatabase21()
+        {
+        }
+
+        public testdatabase21(DbContextOptions<testdatabase21> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Students> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=tcp:testdatabase2.database.windows.net,1433;Initial Catalog=testdatabase21;Persist Security Info=False;User ID=test;Password=Admin1234;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.6-servicing-10079");
+
+            modelBuilder.Entity<Students>(entity =>
+            {
+                entity.Property(e => e.Email).IsUnicode(false);
+
+                entity.Property(e => e.FirstName).IsUnicode(false);
+
+                entity.Property(e => e.LastName).IsUnicode(false);
+
+                entity.Property(e => e.PhoneNumber).IsUnicode(false);
+            });
+        }
+    }
+}
